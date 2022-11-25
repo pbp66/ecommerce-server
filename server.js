91ecda3fa20cde48743ba60241c7b88e98a4d52f
James Perry <53805701+pbp66@users.noreply.github.com>
2022-11-25 16:03:28 -0500

    enable sequelize.sync

--- server.js
+++ server.js
@@ -9,14 +9,13 @@ const enable = true;
 const isProduction = process.env.NODE_ENV === "production";
 const enableForce = isProduction && enable; 
 
-sequelize.sync({ force: enableForce});
-
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.use(routes);
 
 // sync sequelize models to the database, then turn on the server
+sequelize.sync({ force: enableForce});
 app.listen(PORT, () => {
   console.log(`App listening on port ${PORT}!`);
 });
