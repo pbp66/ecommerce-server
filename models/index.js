d2f46a4fa37602ec440d36934e8db8f7e28d05b0
James Perry <53805701+pbp66@users.noreply.github.com>
2022-11-25 14:40:22 -0500

    Implemented model associations and relationships

--- models/index.js
+++ models/index.js
@@ -6,22 +6,22 @@ import ProductTag from './ProductTag';
 
 // Products belongsTo Category
 Product.belongsTo(Category, {
-
+    onDelete: 'CASCADE'
 });
 
 // Categories have many Products
 Category.hasMany(Product, {
-
+    onDelete: 'CASCADE'
 });
 
 // Products belongToMany Tags (through ProductTag)
 Product.belongsToMany(Tag, {
-
+    through: ProductTag
 });
 
 // Tags belongToMany Products (through ProductTag)
-Tag.belongsToMany(Tag, {
-
+Tag.belongsToMany(Product, {
+    through: ProductTag
 });
 
 export { Product, Category, Tag, ProductTag };
