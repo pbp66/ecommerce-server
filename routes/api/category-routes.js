a84e473f8d91cb47ff87168ae846961cac6dd9e9
PBP66 <53805701+pbp66@users.noreply.github.com>
2022-11-26 15:36:19 -0500

    Implemented basic category routes

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -9,7 +9,11 @@ import { Category, Product } from '../../models';
 router.get('/', async (req, res) => {
 	// Return all categories with its linked products
 	try {
-		const categories = await Category.findAll({ include: Product});
+		const categories = await Category.findAll(
+			{ 
+				include: Product
+			}
+			);
 		res.status(200).json(categories).send();
 	} catch(err) {
 		console.log(err);
@@ -22,7 +26,9 @@ router.get('/:id', async (req, res) => {
 	try {
 		const category = await Category.findByPk(
 			req.params.id, 
-			{ include: Product }
+			{ 
+				include: Product 
+			}
 		);
 		res.status(200).json(category).send();
 	} catch(err) {
@@ -31,23 +37,113 @@ router.get('/:id', async (req, res) => {
 	}
 });
 
-router.post('/', (req, res) => {
+router.post('/', async (req, res) => {
 	// create a new category
-	// Category.create()
-	console.log(req.body);
+	/**
+	 * Request body, req.body, JSON format:
+	 * 	{
+	 * 		category_name: "Category"
+	 * 	}
+	 */
+	try {
+		let newCategory = await Category.create(req.body);
+		res.status(201).json(newCategory).send();
+	} catch(err) {
+		console.log(err);
+		res.status(500).send('500 Internal Server Error');
+	}
 });
 
-router.put('/:id', (req, res) => {
+router.post('/:id', async (req, res) => {
+	// create a new category with a specified id
+	/**
+	 * Request body, req.body, JSON format:
+	 * 	{
+	 * 		category_name: "Category"
+	 * 	}
+	 */
+	 try {
+		const newCategory = await Category.create(
+			{
+				id: req.params.id,
+				category_name: req.body.category_name
+			}
+		);
+		res.status(201).json(newCategory).send();
+	} catch(err) {
+		console.log(err);
+		res.status(500).send('500 Internal Server Error');
+	}
+});
+
+router.put('/:id', async (req, res) => {
 	// update a category by its `id` value
-	// Category.update()
-	console.log(req.body);
+	/**
+	 * id needs to be added to body below.
+	 * Request body, req.body, JSON format:
+	 * 	{
+	 * 		category_name: "Category"
+	 * 	}
+	 */
+	const ids = await Category.findAll(
+		{
+			attributes: ['id']
+		}
+	);
+	if (ids.includes(req.params.id)) {
+		try {
+			const updatedCategory = await Category.update(
+				{
+					category_name: req.body.category_name
+				},
+				{ 
+					where: {
+						id: req.params.id
+					}
+				}
+			);
+			// sequelize sends an array of length one with 1 if the entry was updated. Otherwise, it sends a 0 if the entry was not.
+			if (updatedCategory[0]) {
+				res.status(204).send();
+			} else {
+				res.status(304).send();
+			}
+
+		} catch(err) {
+			console.log(err);
+			res.status(500).send('500 Internal Server Error');
+		}
+	} else {
+		try {
+			const newCategory = await Category.create(
+				{
+					id: req.params.id,
+					category_name: req.body.category_name
+				}
+			);
+			res.status(201).json(newCategory).send();
+		} catch(err) {
+			console.log(err);
+			res.status(500).send('500 Internal Server Error');
+		}
+	}
 });
 
-router.delete('/:id', (req, res) => {
+router.delete('/:id', async (req, res) => {
 	// delete a category by its `id` value
-	//Category.destroy()
-	console.log(req.body);
-	console.log(req.params.id);
+	try {
+		await Category.destroy(
+			{ 
+				where: { 
+					id: req.params.id 
+				} 
+			}
+		);
+		res.status(204).send();
+	} catch(err) {
+		console.log(err);
+		res.status(500).send('500 Internal Server Error');
+	}
 });
 
 export default router;
