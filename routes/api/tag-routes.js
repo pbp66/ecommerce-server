c52aeb39b3bfbe2cac3302dd019931baad96aab9
PBP66 <53805701+pbp66@users.noreply.github.com>
2022-11-26 16:48:35 -0500

    Implemented basic routes for tags

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -29,21 +29,113 @@ router.get('/:id', async (req, res) => {
 	}
 });
 
-router.post('/', (req, res) => {
-	console.log(req.body);
+router.post('/', async (req, res) => {
 	// create a new tag
+	/**
+	 * Request body, req.body, JSON format:
+	 * 	{
+	 * 		tag_name: "Tag"
+	 * 	}
+	 */
+	 try {
+		const newTag = await Tag.create(req.body);
+		res.status(201).json(newTag).send();
+	} catch(err) {
+		console.log(err);
+		res.status(500).send('500 Internal Server Error');
+	}
 });
 
-router.put('/:id', (req, res) => {
-	console.log(req.body);
-	console.log(req.params);
+router.post('/:id', async (req, res) => {
+	// create a new tag with a specified id
+	/**
+	 * Request body, req.body, JSON format:
+	 * 	{
+	 * 		tag_name: "Tag"
+	 * 	}
+	 */
+	 try {
+		const newTag = await Tag.create(
+			{
+				id: req.params.id,
+				tag_name: req.body.tag_name
+			}
+		);
+		res.status(201).json(newTag).send();
+	} catch(err) {
+		console.log(err);
+		res.status(500).send('500 Internal Server Error');
+	}
+});
+
+router.put('/:id', async (req, res) => {
 	// update a tag's name by its `id` value
+	/**
+	 * id needs to be added to body below.
+	 * Request body, req.body, JSON format:
+	 * 	{
+	 * 		tag_name: "Tag"
+	 * 	}
+	 */
+	 const ids = await Tag.findAll(
+		{
+			attributes: ['id']
+		}
+	);
+	if (ids.includes(req.params.id)) {
+		try {
+			const updatedTag = await Tag.update(
+				{
+					tag_name: req.body.tag_name
+				},
+				{ 
+					where: {
+						id: req.params.id
+					}
+				}
+			);
+			// sequelize sends an array of length one with 1 if the entry was updated. Otherwise, it sends a 0 if the entry was not.
+			if (updatedTag[0]) {
+				res.status(204).send();
+			} else {
+				res.status(304).send();
+			}
+
+		} catch(err) {
+			console.log(err);
+			res.status(500).send('500 Internal Server Error');
+		}
+	} else {
+		try {
+			const newTag = await Tag.create(
+				{
+					id: req.params.id,
+					tag_name: req.body.tag_name
+				}
+			);
+			res.status(201).json(newTag).send();
+		} catch(err) {
+			console.log(err);
+			res.status(500).send('500 Internal Server Error');
+		}
+	}
 });
 
-router.delete('/:id', (req, res) => {
-	console.log(req.body);
-	console.log(req.params);
+router.delete('/:id', async (req, res) => {
 	// delete on tag by its `id` value
+	try {
+		await Tag.destroy(
+			{ 
+				where: { 
+					id: req.params.id 
+				} 
+			}
+		);
+		res.status(204).send();
+	} catch(err) {
+		console.log(err);
+		res.status(500).send('500 Internal Server Error');
+	}
 });
 
 export default router;
